7e26c7c4402163b853eed26de236819bffc57866
Rodrigo Castro <tfimally@gmail.com>
2021-11-14 01:05:46 -0300

    redireccionar a listar producto admin

--- agregarProductoAdmin.php
+++ agregarProductoAdmin.php
@@ -119,7 +119,7 @@
 					<label class="col-sm-3 control-label">&nbsp;</label>
 					<div class="col-sm-6">
 						<input type="submit" name="add" class="btn btn-sm btn-primary" value="Guardar datos">
-						<a href="mainAdmin.php" class="btn btn-sm btn-danger">Cancelar</a>
+						<a href="listarProductoAdmin.php" class="btn btn-sm btn-danger">Cancelar</a>
 					</div>
 				</div>
 			</form>
